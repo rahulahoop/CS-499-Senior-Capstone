25f4ea7b8fad897fa639d13b7c7f1a16bdfc3e8b
rsb1994 <rbho94@gmail.com>
2015-09-27 11:46:08 -0500

    fixed images path, yellow ball of pacman on screen. made a global pacman variable. new main working file, called game.js

--- pacman/game.js
+++ pacman/game.js
@@ -2,10 +2,13 @@ var game = new Phaser.Game(
     800, 600, Phaser.AUTO, '', 
     { preload: preload, create: create, update: update}
 );
+
+var pacman;
+
 function preload(){
-    game.load.image("pacman","Pacman.png");
-    game.load.spritesheet('ghosts', 'ghost.png', 255, 255,14);
-    game.load.spritesheet('pacmen', 'PacMen.png', 255, 255,12);
+    game.load.image("pacman","images/Pacman.png");
+    game.load.spritesheet('ghosts', 'images/ghost.png', 255, 255,14);
+    game.load.spritesheet('pacman', 'images/PacMen.png', 255, 255,12);
 }
 
 function create(){
@@ -18,25 +21,29 @@ function create(){
 //    pacman = game.add.sprite(game.world.centerx,0,"pacman");
 //    pacman.body.collideWorldBounds = true;
 //    pacman.anchor.setTo(1,1);
+    
+    pacman = game.add.sprite(game.world.centerx,0,"pacman");
+    pacman.scale.setTo(.1,.1);
+    
 }
 
 function update(){
-//    if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-//        pacman.x -= 4;
-//        //pacman.scale.x = -0.1;
-//        pacman.rotation = -3;
-//    }
-//    if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-//        pacman.x += 4;
-//        //pacman.scale.x = 0.1;
-//        pacman.rotation = 0;
-//    }
-//    if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-//        pacman.y += 4;
-//        pacman.rotation = 1.5;
-//    }
-//    if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-//        pacman.y -= 4;
-//        pacman.rotation = -1.5
-//    }
+    if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+        pacman.x -= 4;
+        //pacman.scale.x = -0.1;
+        pacman.rotation = -3;
+    }
+    if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+        pacman.x += 4;
+        //pacman.scale.x = 0.1;
+        pacman.rotation = 0;
+    }
+    if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+        pacman.y += 4;
+        pacman.rotation = 1.5;
+    }
+    if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+        pacman.y -= 4;
+        pacman.rotation = -1.5
+    }
 }
