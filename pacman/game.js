3e526a764105b2aaf6e297e574765a6a74ca768f
rsb1994 <rbho94@gmail.com>
2015-09-27 23:57:53 -0500

    pacman walks and talks like pacman. added the animation and fixed pacmans movement

--- pacman/game.js
+++ pacman/game.js
@@ -1,49 +1,69 @@
+// Game Setup.
 var game = new Phaser.Game(
     800, 600, Phaser.AUTO, '', 
     { preload: preload, create: create, update: update}
 );
 
+// Global Vars.
 var pacman;
+var wall;
 
-function preload(){
-    game.load.image("pacman","images/Pacman.png");
-    game.load.spritesheet('ghosts', 'images/ghost.png', 255, 255,14);
+function preload()
+{
     game.load.spritesheet('pacman', 'images/PacMen.png', 255, 255,12);
+    game.load.image('wallOne', 'images/originalWall.png');
 }
 
-function create(){
-//sample commands
-//    game.stage.backgroundColor = '#ffffff';
-//    ghost = game.add.sprite(256,256,'ghosts',7);
-//    pacmen = game.add.sprite(285,256,'pacmen',2);
-//    ghost.scale.setTo(.1,.1);
-//    pacmen.scale.setTo(.1,.1);
-//    pacman = game.add.sprite(game.world.centerx,0,"pacman");
-//    pacman.body.collideWorldBounds = true;
-//    pacman.anchor.setTo(1,1);
+function create()
+{   
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     
-    pacman = game.add.sprite(game.world.centerx,0,"pacman");
+    // Add pacman sprite, scale, and add anchor.
+    pacman = game.add.sprite(game.world.center,0,"pacman");
     pacman.scale.setTo(.1,.1);
+    pacman.anchor.setTo(.5,.5);
+    
+    // Pacman Position
+    pacman.x = 400
+    pacman.y = 200
+    
+    //Various animations.
+    var death = pacman.animations.add('death');
+    var waka = pacman.animations.add('waka',[0,2,4,2],15,true);
+    
+    // Add walls or map.
+
+    // Add other sprites to physics list.
+    game.physics.enable([pacman], Phaser.Physics.ARCADE);
     
 }
 
-function update(){
+function update()
+{    
+    // Movement Keys.
     if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-        pacman.x -= 4;
-        //pacman.scale.x = -0.1;
+        pacman.x -= 3;
         pacman.rotation = -3;
+        pacman.animations.play('waka');
     }
-    if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-        pacman.x += 4;
-        //pacman.scale.x = 0.1;
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+        pacman.x += 3;
         pacman.rotation = 0;
+        pacman.animations.play('waka');
     }
-    if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-        pacman.y += 4;
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+        pacman.y += 3;
         pacman.rotation = 1.5;
+        pacman.animations.play('waka');
     }
-    if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-        pacman.y -= 4;
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+        pacman.y -= 3;
         pacman.rotation = -1.5
+        pacman.animations.play('waka');
     }
+    else
+    {
+        pacman.animations.stop();
+    }
+
 }
