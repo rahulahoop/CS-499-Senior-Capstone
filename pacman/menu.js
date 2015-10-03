1224b57d8a1bc1435dce26d2789e37ce2ed31012
davidpo <davidpo@users.noreply.github.com>
2015-10-02 20:27:18 -0500

    Update menu.js

--- pacman/menu.js
+++ pacman/menu.js
@@ -5,7 +5,9 @@ var Menu = {
         // The first argument is how our image will be refered to, 
         // the second one is the path to our file.
         Game.load.image('menu','./images/purple gradient.png');
-        Game.load.spritesheet('startButton','./images/PacMen.png', 255, 255,12);
+        Game.load.spritesheet('playGame','./images/playGame.png');
+        Game.load.spritesheet('highScores','./images/highScores.png');
+        Game.load.spritesheet('exitGame','./images/exitGame.png');
     },
 
     create: function (){
@@ -13,12 +15,23 @@ var Menu = {
         menu.scale.setTo(7,6);
         menu.anchor.setTo(.5,.5);
         
-        startButton = Game.add.button(0,0, 'startButton', this.startGame, this);
-        startButton.scale.setTo(.5,.5);
+        // Add Duders Pacman to background
+        dudes = Game.add.text(400, 150, 'Duders Pacman', { font: '65px Arial', fill: '#ffa' });
+        dudes.anchor.setTo(0.5, 0.5);
         
-        startButton.x = 400;
-        startButton.y = 300;
-    
+        // Add buttons - Play Game, High Scores, Quit Game 
+        // All of them have the same function right now - start the game
+        // Scaled to original size, but we can resize them
+        
+        playGame = Game.add.button(300,230, 'playGame', this.startGame, this);
+        playGame.scale.setTo(1,1);
+        
+        highScores = Game.add.button(300,325, 'highScores', this.startGame, this);
+        highScores.scale.setTo(1,1);
+        
+        exitGame = Game.add.button(300,420, 'exitGame', this.startGame, this);
+        exitGame.scale.setTo(1,1);
+       
     },
     
     startGame: function(){
